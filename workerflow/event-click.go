1a465565d310f261317cc361f58f0872a3611e3b
srcrs <srcrs@foxmail.com>
2024-04-14 12:04:37 +0800

    fix node

--- workerflow/event-click.go
+++ workerflow/event-click.go
@@ -3,6 +3,8 @@ package workerflow
 import (
 	"MagicBox/utils"
 	"context"
+	"regexp"
+	"time"
 
 	"github.com/chromedp/chromedp"
 	"github.com/tidwall/gjson"
@@ -14,13 +16,32 @@ func (wf *WorkerFlowData) EventClickExecute(ctx context.Context, workflow, nodeI
 	//当前只有一个判断条件，先写死
 	// waitSelectorTimeout := gjson.Get(workflow, `drawflow.nodes.#(id=="`+nodeId+`").data.waitSelectorTimeout`).String()
 	selector := gjson.Get(workflow, `drawflow.nodes.#(id=="`+nodeId+`").data.selector`).String()
-	if err := chromedp.Run(
-		ctx,
-		chromedp.WaitVisible(selector),
-		chromedp.Click(selector),
-	); err != nil {
-		utils.GLOBAL_LOGGER.Error("event click error: "+err.Error(), zap.String("callid", ctx.Value("callid").(string)))
-		return nil, err
+	utils.GLOBAL_LOGGER.Info("event click selector: "+selector, zap.String("callid", ctx.Value("callid").(string)))
+	itemId := utils.GetVariableName(selector)
+	if len(wf.LoopDataElements[itemId]) > 0 {
+		re := regexp.MustCompile(`{{loopData@.*}}\s+`)
+		selector = re.ReplaceAllString(selector, "")
+		selector = utils.CssToXpath(selector)
+		selector = wf.LoopDataElements[itemId][0].FullXPath() + selector
+		utils.GLOBAL_LOGGER.Info("click element : "+selector, zap.String("callid", ctx.Value("callid").(string)))
+		if err := chromedp.Run(
+			ctx,
+			chromedp.WaitVisible(selector),
+			chromedp.Click(selector),
+			chromedp.Sleep(2*time.Second),
+		); err != nil {
+			utils.GLOBAL_LOGGER.Error("click element : "+err.Error(), zap.String("callid", ctx.Value("callid").(string)))
+		}
+	} else {
+		if err := chromedp.Run(
+			ctx,
+			chromedp.Sleep(2*time.Second),
+			chromedp.WaitVisible(selector),
+			chromedp.Click(selector),
+		); err != nil {
+			utils.GLOBAL_LOGGER.Error("event click error: "+err.Error(), zap.String("callid", ctx.Value("callid").(string)))
+			return nil, err
+		}
 	}
 
 	return nil, nil
--- workerflow/get-text.go
+++ workerflow/get-text.go
@@ -3,44 +3,38 @@ package workerflow
 import (
 	"MagicBox/utils"
 	"context"
-	"fmt"
 	"regexp"
+	"strings"
 
-	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/chromedp"
 	"github.com/tidwall/gjson"
 	"go.uber.org/zap"
 )
 
 func (wf *WorkerFlowData) GettextExecute(ctx context.Context, workflow, nodeId string) (interface{}, error) {
-	// xxx := &LoopdataStrategy{}
-	// findBy := gjson.Get(workflow, `drawflow.nodes.#(id=="`+nodeId+`").data.findBy`).String()
-	// waitForSelector := gjson.Get(workflow, `drawflow.nodes.#(id=="`+nodeId+`").data.waitForSelector`).Bool()
-	// waitSelectorTimeout := gjson.Get(workflow, `drawflow.nodes.#(id=="`+nodeId+`").data.waitSelectorTimeout`).Int()
 	selector := gjson.Get(workflow, `drawflow.nodes.#(id=="`+nodeId+`").data.selector`).String()
 	variableName := gjson.Get(workflow, `drawflow.nodes.#(id=="`+nodeId+`").data.variableName`).String()
-	// dataColumn := gjson.Get(workflow, `drawflow.nodes.#(id=="`+nodeId+`").data.dataColumn`).String()
+	// findBy := gjson.Get(workflow, `drawflow.nodes.#(id=="`+nodeId+`").data.findBy`).String()
 	itemId := utils.GetVariableName(selector)
 	if len(wf.LoopDataElements[itemId]) > 0 {
 		re := regexp.MustCompile(`{{loopData@.*}}\s+`)
 		selector = re.ReplaceAllString(selector, "")
+		selector = utils.CssToXpath(selector)
+		selector = wf.LoopDataElements[itemId][0].FullXPath() + selector
+		utils.GLOBAL_LOGGER.Info("get text selector: "+selector, zap.String("callid", ctx.Value("callid").(string)))
+		all := ""
+		if err := chromedp.Run(
+			ctx,
 
-		var chrnodes []*cdp.Node
-
-		for _, node := range wf.LoopDataElements[itemId] {
-			chromedp.Run(
-				ctx,
-				chromedp.Nodes(selector, &chrnodes, chromedp.FromNode(node)),
-			)
+			chromedp.WaitVisible(selector),
+			chromedp.TextContent(selector, &all),
+		); err != nil {
+			utils.GLOBAL_LOGGER.Error("get text : "+err.Error(), zap.String("callid", ctx.Value("callid").(string)))
 		}
-		for _, node := range chrnodes {
-			all := ""
-			chromedp.Run(
-				ctx,
-				chromedp.TextContent(node.FullXPath(), &all),
-			)
-			fmt.Println(all)
-		}
+		all = strings.Replace(all, "\n", "", -1)
+		all = strings.Replace(all, "\t", "", -1)
+		utils.GLOBAL_LOGGER.Info("get text : "+all+" selector: "+wf.LoopDataElements[itemId][0].FullXPath()+" "+selector, zap.String("callid", ctx.Value("callid").(string)))
+		wf.VariableMap["{{variables@"+variableName+"}}"] = all
 	} else {
 		textContent := ""
 		if err := chromedp.Run(
