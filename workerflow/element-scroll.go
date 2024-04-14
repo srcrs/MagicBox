9ff559ae00a3e1ff7da2ecdd26ab423920089adf
srcrs <srcrs@foxmail.com>
2024-04-14 11:54:34 +0800

    add element-scroll node

---
+++ workerflow/element-scroll.go
@@ -0,0 +1,30 @@
+package workerflow
+
+import (
+	"MagicBox/utils"
+	"context"
+	"time"
+
+	"github.com/chromedp/chromedp"
+	"go.uber.org/zap"
+)
+
+//滑动页面到底部
+func (wf *WorkerFlowData) ElementScrollExecute(ctx context.Context, workflow, nodeId string) (interface{}, error) {
+	// selector := gjson.Get(workflow, `drawflow.nodes.#(id=="`+nodeId+`").data.selector`).String()
+	// waitSelectorTimeout := gjson.Get(workflow, `drawflow.nodes.#(id=="`+nodeId+`").data.waitSelectorTimeout`).Int()
+	// chromdpCtx, cancel := context.WithTimeout(ctx, time.Duration(waitSelectorTimeout)*time.Millisecond)
+	// defer cancel()
+	if err := chromedp.Run(
+		ctx,
+		chromedp.Sleep(3*time.Second),
+		// chromedp.WaitVisible(selector),
+		chromedp.EvaluateAsDevTools(`window.scrollTo(0, document.body.scrollHeight);`, nil),
+		chromedp.Sleep(1*time.Second),
+	); err != nil {
+		utils.GLOBAL_LOGGER.Error("element-scroll error: "+err.Error(), zap.String("callid", ctx.Value("callid").(string)))
+		return nil, err
+	}
+
+	return nil, nil
+}
