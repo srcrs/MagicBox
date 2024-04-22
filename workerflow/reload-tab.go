0be0c2ed6f97873c4f2c895ca19be220c7df6278
srcrs <srcrs@foxmail.com>
2024-04-23 01:32:10 +0800

    刷新当前页面

---
+++ workerflow/reload-tab.go
@@ -0,0 +1,23 @@
+package workerflow
+
+import (
+	"MagicBox/utils"
+	"context"
+
+	"github.com/chromedp/chromedp"
+	"go.uber.org/zap"
+)
+
+//刷新当前页面
+func (wf *WorkerFlowData) ReloadTabExecute(ctx context.Context, workflow, nodeId string) (interface{}, error) {
+	utils.GLOBAL_LOGGER.Info("reload tab", zap.String("callid", ctx.Value("callid").(string)))
+
+	if err := chromedp.Run(
+		ctx,
+		chromedp.Reload(),
+	); err != nil {
+		utils.GLOBAL_LOGGER.Error("forms set value: "+err.Error(), zap.String("callid", ctx.Value("callid").(string)))
+	}
+
+	return nil, nil
+}
