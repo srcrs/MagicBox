8c1ee6c0ddd3001425b4b6d19805ec57ef426325
srcrs <srcrs@foxmail.com>
2024-05-10 01:33:52 +0800

    fix automa no go back

--- workerflow/close-tab.go
+++ workerflow/close-tab.go
@@ -3,21 +3,33 @@ package workerflow
 import (
 	"MagicBox/utils"
 	"context"
+	"time"
 
 	"github.com/chromedp/cdproto/page"
 	"github.com/chromedp/chromedp"
 	"go.uber.org/zap"
 )
 
-//刷新当前页面
+// 关闭当前页面
 func (wf *WorkerFlowData) CloseTabExecute(ctx context.Context, workflow, nodeId string) (interface{}, error) {
 	utils.GLOBAL_LOGGER.Info("close tab", zap.String("callid", ctx.Value("callid").(string)))
-
-	if err := chromedp.Run(
-		ctx,
-		page.Close(),
-	); err != nil {
-		utils.GLOBAL_LOGGER.Error("reload tab: "+err.Error(), zap.String("callid", ctx.Value("callid").(string)))
+	if wf.CloseTabIsGoBack {
+		if err := chromedp.Run(
+			ctx,
+			chromedp.Sleep(3*time.Second),
+			chromedp.Evaluate(`history.go(-1)`, nil),
+			chromedp.Sleep(3*time.Second),
+		); err != nil {
+			utils.GLOBAL_LOGGER.Error("close tab revise goBackPage: "+err.Error(), zap.String("callid", ctx.Value("callid").(string)))
+		}
+		wf.CloseTabIsGoBack = false
+	} else {
+		if err := chromedp.Run(
+			ctx,
+			page.Close(),
+		); err != nil {
+			utils.GLOBAL_LOGGER.Error("close tab: "+err.Error(), zap.String("callid", ctx.Value("callid").(string)))
+		}
 	}
 
 	return nil, nil
--- workerflow/commondata.go
+++ workerflow/commondata.go
@@ -1,10 +1,14 @@
 package workerflow
 
-import "github.com/chromedp/cdproto/cdp"
+import (
+	"github.com/chromedp/cdproto/cdp"
+)
 
 type WorkerFlowData struct {
 	LoopDataElements map[string][]*cdp.Node
 	VariableMap      map[string]string
 	NextNodeId       string
 	ResetNextNodeId  string
+	//为了兼容automa无法返回上一页问题
+	CloseTabIsGoBack bool
 }
